3bce8e4c6000ba62906c843bdc6433f591879753
Ezioquyen <quyen.tdv.work@gmail.com>
2024-04-23 21:16:52 +0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Levents loves you &lt;3 </header>
+    </div>
+  );
+}
+
+export default App;
